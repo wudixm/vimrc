83ae0b8db5c7dac755e306d923f50fd624ae73bb
xiaoming.wu <xiaoming.wu@colorv.com>
2021-11-25 10:29:36 +0800

    add c-a/e/k/f/b/d/ stuff to ideavim

--- init.el
+++ init.el
@@ -371,13 +371,23 @@ me-mode 1)))
   :custom
   (org-roam-directory "/Users/wuxiaoming/local/tempFiles/org_roam")
   (org-roam-completion-everywhere t)
+  (org-roam-dailies-capture-templates
+    '(("d" "default" entry "* %<%I:%M %p>: %?"
+       :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n i" . org-roam-node-insert)
 	 :map org-mode-map
          ("C-M-i"    . completion-at-point)
+	  :map org-roam-dailies-map
+         ("Y" . org-roam-dailies-capture-yesterday)
+         ("T" . org-roam-dailies-capture-tomorrow)
 	 )
+  :bind-keymap
+  ("C-c n d" . org-roam-dailies-map)
   :config
+  (require 'org-roam-dailies) ;; Ensure the keymap is available
+  (org-roam-db-autosync-mode)
   (org-roam-setup))
 
 (setq org-roam-node-display-template "${title} ${tags}")
